1ea185e04b672f28a5d2e173796a94becee4a1dd
Sara Spink <spinkbot@gmail.com>
2017-12-01 12:16:44 -0800

    set up factory for user and pass test to validate presence of email and password

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :user do
-    
+    email('unicorn_lover@gmail.com')
+    password('123456')
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+# RSpec.describe User, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
+
+describe User do
+  it 'has a name, email, and image' do
+    user = FactoryBot.create(:user)
+    user.email.should eq 'unicorn_lover@gmail.com'
+    user.password.should eq '123456'
+  end
 end
