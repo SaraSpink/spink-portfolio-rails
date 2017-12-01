13cf5912f9ed46c0620d2fc7d08845ddf0e4bf03
Sara Spink <spinkbot@gmail.com>
2017-12-01 15:48:45 -0800

    add permissions and update user edit page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,29 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def authorize
+    if !current_user
+      flash[:alert] = "Please sign in to visit this page."
+      redirect_to '/'
+    end
+  end
+  #
+  # def authorize_admin
+  #
+  #   if current_user.admin === true
+  #     new_location_path
+  #   else
+  #     flash[:alert] = "Only an admin can visit this page."
+  #     redirect_to '/'
+  #   end
+  # end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :avatar) }
+  end
 end
